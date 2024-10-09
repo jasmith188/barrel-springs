19901270aa2140d2567f3eea8dc39637fdadc79f
jasmith188 <jasmith.188@gmail.com>
2024-10-09 00:26:36 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,36 @@ const Footer = () => {
         <div>
           <h3 className="text-white text-lg font-semibold mb-4">About Us</h3>
           <p className="text-gray-400">
-            Come on down to BS. Barrel Springs Bar & Grill
+            <div className="flex space-x-4 mb-4 justify-content-center md:mb-0">
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a
+                href="https://instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-400 hover:text-white">
+                <i className="fab fa-twitter"></i>
+              </a>
+            </div>
           </p>
         </div>
 
         {/* Column 2: Quick Links */}
         <div>
           <h3 className="text-white text-lg font-semibold mb-4">Quick Links</h3>
-          <ul>
+          <ul className="p-0">
             <li className="mb-2">
               <a
                 href="https://acrobat.adobe.com/id/urn:aaid:sc:US:7853c796-ff9d-46a5-976c-c7d4d0f0fa08"
@@ -24,64 +46,44 @@ const Footer = () => {
               </a>
             </li>
             <li className="mb-2">
-              <a href="#about" className="text-gray-400 hover:text-white">
-                About Us
-              </a>
-            </li>
-            <li className="mb-2">
-              <a href="#contact" className="text-gray-400 hover:text-white">
+              <a
+                href="#contact"
+                className="text-gray-400 hover:text-white no-underline">
                 Contact
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#reservation" className="text-gray-400 hover:text-white">
                 Reservations
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
 
         {/* Column 3: Contact Info */}
         <div>
           <h3 className="text-white text-lg font-semibold mb-4">Contact Us</h3>
-          <p className="text-gray-400">123 Food Street, Wisconsin, USA</p>
-          <p className="text-gray-400">Phone: (123) 456-7890</p>
-          <p className="text-gray-400">Email: info@restaurant.com</p>
+          <p className="text-gray-400">999 Highway 82 Wisconsin Dells, WI</p>
+          <a className="no-underline " href="tel:+6082536888">
+            <p className="text-gray-400 hover:text-white">(608) 253-6888</p>
+          </a>
+          <a className="no-underline" href="mailto:barrelspringsbg@gmail.com">
+            <p className="text-gray-400 hover:text-white">
+              barrelspringsbg@gmail.com
+            </p>
+          </a>
         </div>
       </div>
 
       {/* Bottom Section: Social Media & Copyright */}
-      <div className="border-t border-gray-700 mt-10 pt-5">
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-5">
+      <div className="border-t border-gray-700 mt-10 pt-5 text center">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-5 justify-content-center">
           {/* Social Media Links */}
-          <div className="flex space-x-4 mb-4 md:mb-0">
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 hover:text-white">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 hover:text-white">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 hover:text-white">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </div>
 
           {/* Copyright */}
           <p className="text-gray-400">
-            &copy; {new Date().getFullYear()} [Your Restaurant Name]. All rights
-            reserved.
+            &copy; {new Date().getFullYear()} Barrel Springs Bar & Grill. All
+            rights reserved. Website created by The Chef.
           </p>
         </div>
       </div>
